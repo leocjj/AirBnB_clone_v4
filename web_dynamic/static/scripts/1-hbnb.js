7d69d3af1db613202451623774f58abaff03435a
ilasso <1204@holbertonschool.com>
2020-05-16 16:44:41 +0000

    dynamic:task1, to test into container

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,20 @@
+let checked = {};
+$(document).ready(function () {
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      checked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checked[$(this).data('id')];
+    }
+    $('div.amenities h4').html(function () {
+      let amenities = [];
+      Object.keys(checked).forEach(function (key) {
+        amenities.push(checked[key]);
+      });
+      if (amenities.length === 0) {
+        return ('&nbsp');
+      }
+      return (amenities.join(', '));
+    });
+  });
+});
