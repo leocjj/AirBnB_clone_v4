0a2141f18d458a9777e9494dabad4dd5a9924df6
leocjj <leocj@hotmail.com>
2020-05-16 14:31:13 -0500

    New get function to get/set status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+let checked = {};
+$(document).ready(function () {
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      checked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checked[$(this).data('id')];
+    }
+    $('div.amenities h4').html(function () {
+      let amenities = [];
+      Object.keys(checked).forEach(function (key) {
+        amenities.push(checked[key]);
+      });
+      if (amenities.length === 0) {
+        return ('&nbsp');
+      }
+      return (amenities.join(', '));
+    });
+  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
