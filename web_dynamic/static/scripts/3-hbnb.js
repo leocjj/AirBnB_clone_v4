8ce0865d4c03d398785a82a63d740ad423092e49
ilasso <1204@holbertonschool.com>
2020-05-17 22:37:07 +0000

    dynamic:include username

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,3 +1,4 @@
+// when the user check an amenity put into amenities div
 let checked = {};
 $(document).ready(function () {
   $('input:checkbox').change(function () {
@@ -19,6 +20,8 @@ $(document).ready(function () {
   });
 });
 
+// to get status to display signal light into web page
+
 $.get('http://374566e96a79.19.hbtn-cod.io:37218/api/v1/status/', function (data, status) {
   if (data.status === 'OK') {
     $('DIV#api_status').addClass('available');
@@ -27,14 +30,8 @@ $.get('http://374566e96a79.19.hbtn-cod.io:37218/api/v1/status/', function (data,
   }
 });
 
-/*$.post('http://05c25b3efdc0.19.hbtn-cod.io:37166/api/v1/places_search/', 
-  { },
-  function (data, status) {
-     console.log('estado '+status);
-    for (let i of data) {
-$('.places').append('<article> <div class="title"> <h2>' + i.name + '</h2><div class="price_by_night">' + '$' + i.price_by_night + '</div></div> <div class="information"> <div class="max_guest"> <i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + i.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + i.number_rooms + ' Bedrooms </div> <div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + i.number_bathrooms + ' Bathroom  </div></div> <div class="user"></div><div class="description">' + i.description + '</div></article>');
-    }
-},"json");*/
+
+// get places to display
 
 $.ajax({
   type: 'POST',
@@ -43,9 +40,7 @@ $.ajax({
   data: '{}',
   success: function (data) {
     for (let currentPlace of data) {
-     // $('.places').append('<article> <div class="price_title"> <h2>' + currentPlace.name + '</h2><div class="price_by_night">' + '$' + currentPlace.price_by_night + '</div></div> <div class="information"> <div class="max_guest"> <i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + currentPlace.max_guest + ' Guests</div><div class="number_rooms"><i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + currentPlace.number_rooms + ' Bedrooms </div> <div class="number_bathrooms"><i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + currentPlace.number_bathrooms + ' Bathroom  </div></div> <div class="user"></div><div class="description">' + currentPlace.description + '</div></article>');
-
-        $('.places .articles').append('<ARTICLE> <DIV class="price_title"> <DIV class="article_title"> <H2>' + currentPlace.name + '</H2> </DIV> <DIV class="price_by_night"> <DIV class="price"> $' + currentPlace.price_by_night + '</DIV></DIV></DIV><DIV class="information"><DIV class="max_guest"> <DIV class="guest_icon"></DIV><DIV class="guest_number"> <H2>'+currentPlace.max_guest + ' Guest</H2> </DIV> </DIV> <DIV class="number_rooms"> <DIV class="rooms_icon"> </DIV> <DIV class="rooms_number"> <H2>'+  currentPlace.number_rooms + ' Room</H2> </DIV> </DIV> <DIV class="number_bathrooms"> <DIV class="bathrooms_icon"> </DIV> <DIV class="bathrooms_number"> <H2>' + currentPlace.number_bathrooms + ' Bathroom</H2> </DIV> </DIV> </DIV> <DIV class="user"> <B>Owner: </B>{{ place.user.first_name }} {{ place.user.last_name }} </DIV> <DIV class="description">'+  currentPlace.description+' </DIV> </ARTICLE>');
+        $('.places .articles').append('<ARTICLE> <DIV class="price_title"> <DIV class="article_title"> <H2>' + currentPlace.name + '</H2> </DIV> <DIV class="price_by_night"> <DIV class="price"> $' + currentPlace.price_by_night + '</DIV></DIV></DIV><DIV class="information"><DIV class="max_guest"> <DIV class="guest_icon"></DIV><DIV class="guest_number"> <H2>'+currentPlace.max_guest + ' Guest</H2> </DIV> </DIV> <DIV class="number_rooms"> <DIV class="rooms_icon"> </DIV> <DIV class="rooms_number"> <H2>'+  currentPlace.number_rooms + ' Room</H2> </DIV> </DIV> <DIV class="number_bathrooms"> <DIV class="bathrooms_icon"> </DIV> <DIV class="bathrooms_number"> <H2>' + currentPlace.number_bathrooms + ' Bathroom</H2> </DIV> </DIV> </DIV> <DIV class="user"> <B>Owner: </B> '+ currentPlace.username + ' </DIV> <DIV class="description">'+  currentPlace.description+' </DIV> </ARTICLE>');
     }
   }
 });
