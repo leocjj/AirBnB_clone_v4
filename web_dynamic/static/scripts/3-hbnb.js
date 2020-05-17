a160956b24bf5c6209aec72fe8f29f02f6393c56
leocjj <leocj@hotmail.com>
2020-05-17 18:55:00 -0500

    semistandard style

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,5 +1,5 @@
 // when the user check an amenity put into amenities div
-let checked = {};
+const checked = {};
 $(document).ready(function () {
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
@@ -8,7 +8,7 @@ $(document).ready(function () {
       delete checked[$(this).data('id')];
     }
     $('div.amenities h4').html(function () {
-      let amenities = [];
+      const amenities = [];
       Object.keys(checked).forEach(function (key) {
         amenities.push(checked[key]);
       });
@@ -30,7 +30,6 @@ $.get('http://374566e96a79.19.hbtn-cod.io:37218/api/v1/status/', function (data,
   }
 });
 
-
 // get places to display
 
 $.ajax({
@@ -39,8 +38,8 @@ $.ajax({
   contentType: 'application/json',
   data: '{}',
   success: function (data) {
-    for (let currentPlace of data) {
-        $('.places .articles').append('<ARTICLE> <DIV class="price_title"> <DIV class="article_title"> <H2>' + currentPlace.name + '</H2> </DIV> <DIV class="price_by_night"> <DIV class="price"> $' + currentPlace.price_by_night + '</DIV></DIV></DIV><DIV class="information"><DIV class="max_guest"> <DIV class="guest_icon"></DIV><DIV class="guest_number"> <H2>'+currentPlace.max_guest + ' Guest</H2> </DIV> </DIV> <DIV class="number_rooms"> <DIV class="rooms_icon"> </DIV> <DIV class="rooms_number"> <H2>'+  currentPlace.number_rooms + ' Room</H2> </DIV> </DIV> <DIV class="number_bathrooms"> <DIV class="bathrooms_icon"> </DIV> <DIV class="bathrooms_number"> <H2>' + currentPlace.number_bathrooms + ' Bathroom</H2> </DIV> </DIV> </DIV> <DIV class="user"> <B>Owner: </B> '+ currentPlace.username + ' </DIV> <DIV class="description">'+  currentPlace.description+' </DIV> </ARTICLE>');
+    for (const currentPlace of data) {
+      $('.places .articles').append('<ARTICLE> <DIV class="price_title"> <DIV class="article_title"> <H2>' + currentPlace.name + '</H2> </DIV> <DIV class="price_by_night"> <DIV class="price"> $' + currentPlace.price_by_night + '</DIV></DIV></DIV><DIV class="information"><DIV class="max_guest"> <DIV class="guest_icon"></DIV><DIV class="guest_number"> <H2>' + currentPlace.max_guest + ' Guest</H2> </DIV> </DIV> <DIV class="number_rooms"> <DIV class="rooms_icon"> </DIV> <DIV class="rooms_number"> <H2>' + currentPlace.number_rooms + ' Room</H2> </DIV> </DIV> <DIV class="number_bathrooms"> <DIV class="bathrooms_icon"> </DIV> <DIV class="bathrooms_number"> <H2>' + currentPlace.number_bathrooms + ' Bathroom</H2> </DIV> </DIV> </DIV> <DIV class="user"> <B>Owner: </B> ' + currentPlace.username + ' </DIV> <DIV class="description">' + currentPlace.description + ' </DIV> </ARTICLE>');
     }
   }
 });
